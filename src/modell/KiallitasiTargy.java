cab9e9e747c9bf4513d9758ad672440534e637b8
KNikooo <kissnikolettanett@gmail.com>
2022-04-20 11:03:40 +0200

    KiallitasiTargy constructor elkeszitese

--- src/modell/KiallitasiTargy.java
+++ src/modell/KiallitasiTargy.java
@@ -1,5 +1,23 @@
 package modell;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public abstract class KiallitasiTargy {
+    private String keszito, cim;
+    private Date letrehozasiDatum;
+
+    public KiallitasiTargy(String keszito, String cim) {
+        //this(keszito, cim, );
+        this.keszito = keszito;
+        this.cim = cim;
+    }
     
+    
+
+    public KiallitasiTargy(String keszito, String cim, Date letrehozasiDatum) {
+        this.keszito = keszito;
+        this.cim = cim;
+        this.letrehozasiDatum = letrehozasiDatum;
+    }
 }
